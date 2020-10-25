c1a57b4159ae975470b220b9723562949f210970
taegeon-park23 <justerror23@gmail.com>
2020-10-25 14:54:02 +0900

    test-traceLot

---
+++ src/Components/TraceLot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TraceLot extends React.Component {
+    render() {
+        return(
+            <div id="TraceLot">
+                example
+            </div>
+        );
+    }
+}
